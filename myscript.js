ab776b4ffcbf620e6fa4da1f7267ae7ac77293bb
reymart <reymartbara20@gmail.com>
2024-05-12 09:52:33 +0800

    update ang api kay wa ni gana

--- myscript.js
+++ myscript.js
@@ -1,19 +1,38 @@
-
-
-async function getAnime() {
-    const response = await fetch('https://animechan.xyz/api/random')
-    const data = await response.json();
-    let quote = document.getElementById('quote');
-    let anime = document.getElementById('anime-name');
-    let character = document.getElementById('anime-character');
-    quote.innerHTML = data.quote;
-    anime.innerHTML = `anime: ${data.anime} - `;
-    character.innerHTML = data.character;
-    
-}
-
-getAnime();
+fetchanimequotes();
 
 function newQuotes() {
-    getAnime();
+    fetchanimequotes();
 }
+
+async function fetchanimequotes() {
+    const headers = new Headers({
+      'Authorization': 'NzMyOTQ2OTEyNDExNTE2OTQ4.MTcxNTQzNTUxNg--.38e22df28'
+    });
+  
+    try {
+      const response = await fetch('https://waifu.it/api/v4/quote', {
+        method: 'GET',
+        headers: headers
+      });
+  
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+  
+      const data = await response.json();
+
+      let quote = document.getElementById('quote');
+      let anime = document.getElementById('anime-name');
+      let character = document.getElementById('anime-character');
+
+      quote.innerHTML = data.quote;
+      anime.innerHTML = `anime: ${data.anime} `;
+      character.innerHTML = ` - ${data.author}`;
+      console.log(data);
+
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error; // Or handle the error differently
+    }
+}
+
